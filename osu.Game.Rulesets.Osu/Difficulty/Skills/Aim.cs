ce7e0e71cf40f09840f38ec79a58fe4939d43114
Khasyfi <Khasyfiabu@gmail.com>
2026-03-26 02:41:58 +0800

    change scaling again and unnerf speedflow

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -33,7 +33,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double skillMultiplierSnap => 71.0;
         private double skillMultiplierAgility => 2.35;
-        private double skillMultiplierFlow => 205.0;
+        private double skillMultiplierFlow => 197.0;
         private double skillMultiplierTotal => 1.11;
         private double meanExponent => 1.2;
 
