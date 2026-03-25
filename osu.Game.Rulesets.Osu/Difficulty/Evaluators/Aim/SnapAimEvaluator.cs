2a5a0a7668b6075dfd2b6044ab2f115ef4dd8109
Khasyfi <Khasyfiabu@gmail.com>
2026-03-26 00:40:38 +0800

    nerf slider

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/SnapAimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/SnapAimEvaluator.cs
@@ -14,7 +14,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
     {
         private const double wide_angle_multiplier = 1.05;
         private const double acute_angle_multiplier = 2.41;
-        private const double slider_multiplier = 1.5;
+        private const double slider_multiplier = 1.15;
         private const double velocity_change_multiplier = 0.9;
         private const double wiggle_multiplier = 1.02; // WARNING: Increasing this multiplier beyond 1.02 reduces difficulty as distance increases. Refer to the desmos link above the wiggle bonus calculation
         private const double maximum_repetition_nerf = 0.15;
