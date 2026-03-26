d5b013348edd6f6f9dc956cbf4bb31b2aff8d3bd
Khasyfi <Khasyfiabu@gmail.com>
2026-03-27 00:17:42 +0800

    Remove the 2

--- osu.Game.Rulesets.Osu/Difficulty/OsuRatingCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuRatingCalculator.cs
@@ -29,7 +29,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             if (mods.Any(m => m is OsuModAutopilot))
                 return 0;
 
-            double aimRating = Math.Pow(2 * aimDifficultyValue, 0.7) * 0.01035;
+            double aimRating = Math.Pow(aimDifficultyValue, 0.7) * 0.01672;
 
             if (mods.Any(m => m is OsuModRelax))
                 aimRating *= 0.9;
