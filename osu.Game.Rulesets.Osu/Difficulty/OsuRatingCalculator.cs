7b229d8215b20b7f3398dabf4a8b301d0e7e68e2
Khasyfi <Khasyfiabu@gmail.com>
2026-03-29 01:47:50 +0800

    change scaling again

--- osu.Game.Rulesets.Osu/Difficulty/OsuRatingCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuRatingCalculator.cs
@@ -29,7 +29,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             if (mods.Any(m => m is OsuModAutopilot))
                 return 0;
 
-            double aimRating = Math.Pow(aimDifficultyValue, 0.7) * 0.01672;
+            double aimRating = Math.Pow(aimDifficultyValue, 0.7) * 0.0168;
 
             if (mods.Any(m => m is OsuModRelax))
                 aimRating *= 0.9;
