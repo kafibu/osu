e248aff52d3c194d912bb3ddeb65ce10fd69f876
Khasyfi <Khasyfiabu@gmail.com>
2026-07-15 17:50:08 +0800

    Revert "Merge branch 'stat-acc+d/t+fctime' of https://github.com/kafibu/osu into stat-acc+d/t+fctime"
    This reverts commit 87eab682c324c870fb25ccb057654a80135dfab5, reversing
    changes made to abba474b0dcfdd1db4f969585a9528093c2a245e.

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -18,7 +18,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// <summary>
     /// Represents the skill required to correctly aim at every object in the map with a uniform CircleSize and normalized distances.
     /// </summary>
-    public class Aim : TimeSkill
+    public class Aim : VariableLengthStrainSkill
     {
         public readonly bool IncludeSliders;
 
@@ -30,23 +30,23 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double currentStrain;
 
-        private double skillMultiplierSnap => 355.0;
-        private double skillMultiplierAgility => 10.0;
-        private double skillMultiplierFlow => 850;
-        private double skillMultiplierTotal => 1.05;
-        private double meanExponent => 1.2;
+        /// <summary>
+        /// The number of sections with the highest strains, which the peak strain reductions will apply to.
+        /// This is done in order to decrease their impact on the overall difficulty of the map for this skill.
+        /// </summary>
+        private int reducedSectionTime => 4000;
+
+        /// <summary>
+        /// The baseline multiplier applied to the section with the biggest strain.
+        /// </summary>
+        private const double reduced_strain_baseline = 0.727;
 
         private readonly List<double> sliderStrains = new List<double>();
 
-        protected override double HitProbability(double skill, double difficulty)
-        {
-            if (difficulty <= 0) return 1;
-            if (skill <= 0) return 0;
+        private double strainDecay(double ms) => DiffUtils.Pow(0.2, ms / 1000);
 
-            return DifficultyCalculationUtils.Erf(skill / (Math.Sqrt(2) * difficulty));
-        }
-
-        private double strainDecay(double ms) => Math.Pow(0.15, ms / 1000);
+        protected override double CalculateInitialStrain(double time, DifficultyHitObject current) =>
+            currentStrain * strainDecay(time - current.Previous(0).StartTime);
 
         protected override double StrainValueAt(DifficultyHitObject current)
         {
@@ -142,6 +142,85 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
             // Use a weighted sum of all strains. Constants are arbitrary and give nice values
             return sliderStrains.Sum(s => DifficultyCalculationUtils.Logistic(s / consistentTopStrain, 0.88, 10, 1.1));
+        }
+
+        public override double DifficultyValue()
+        {
+            double difficulty = 0;
+            double time = 0;
+
+            var strains = getReducedStrainPeaks();
+
+            // Difficulty is a continuous weighted sum of the sorted strains
+            foreach (StrainPeak strain in strains)
+            {
+                /* Weighting function can be thought of as:
+                        b
+                        ∫ DecayWeight^x dx
+                        a
+                    where a = startTime and b = endTime
+
+                    Technically, the function below has been slightly modified from the equation above.
+                    The real function would be
+                        double weight = DiffUtils.Pow(DecayWeight, startTime) - DiffUtils.Pow(DecayWeight, endTime);
+                        ...
+                        return difficulty / Math.Log(1 / DecayWeight);
+                    E.g. for a DecayWeight of 0.9, we're multiplying by 10 instead of 9.49122...
+
+                    This change makes it so that a map composed solely of MaxSectionLength chunks will have the exact same value when summed in this class and StrainSkill.
+                    Doing this ensures the relationship between strain values and difficulty values remains the same between the two classes.
+                */
+                double startTime = time;
+                double endTime = time + strain.SectionLength / MaxSectionLength;
+
+                double weight = DiffUtils.Pow(DecayWeight, startTime) - DiffUtils.Pow(DecayWeight, endTime);
+
+                difficulty += strain.Value * weight;
+                time = endTime;
+            }
+
+            return difficulty / (1 - DecayWeight);
+        }
+
+        /// <summary>
+        /// Returns a sorted enumerable of strain peaks with the highest values reduced.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<StrainPeak> getReducedStrainPeaks()
+        {
+            // Sections with 0 strain are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
+            // These sections will not contribute to the difficulty.
+
+            List<StrainPeak> strains = GetCurrentStrainPeaks()
+                                       .Where(p => p.Value > 0)
+                                       .ToList();
+
+            const int chunk_size = 20;
+            double time = 0;
+            int skipCount = 0;
+
+            // We are reducing the highest strains first to account for extreme difficulty spikes
+            // Strains are split into 20ms chunks to try to mitigate inconsistencies caused by reducing strains
+            while (strains.Count > skipCount && time < reducedSectionTime)
+            {
+                StrainPeak strain = strains[skipCount];
+
+                for (double addedTime = 0; addedTime < strain.SectionLength; addedTime += chunk_size)
+                {
+                    double scale = Math.Log10(Interpolation.Lerp(1, 10, Math.Clamp((time + addedTime) / reducedSectionTime, 0, 1)));
+
+                    // intentionally add at end and sort afterwards, should be cheaper.
+                    strains.Add(new StrainPeak(
+                        strain.Value * Interpolation.Lerp(reduced_strain_baseline, 1.0, scale),
+                        Math.Min(chunk_size, strain.SectionLength - addedTime)
+                    ));
+                }
+
+                time += strain.SectionLength;
+                skipCount++;
+            }
+
+            return strains.Skip(skipCount).OrderByDescending(p => p.Value);
         }
     }
 }
